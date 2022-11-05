955ab9af9b767e1b907f1f539acb5ef7d26ccaad
Gu <victorgw@amazon.com>
2022-11-04 21:49:08 -0500

    update module to4.15

--- schedulers/argo-workflow/addons.tf
+++ schedulers/argo-workflow/addons.tf
@@ -1,5 +1,5 @@
 module "eks_blueprints_kubernetes_addons" {
-  source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons"
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons?ref=v4.15.0"
 
   eks_cluster_id       = module.eks_blueprints.eks_cluster_id
   eks_cluster_endpoint = module.eks_blueprints.eks_cluster_endpoint
