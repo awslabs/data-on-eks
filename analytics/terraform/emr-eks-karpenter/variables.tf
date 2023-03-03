cbb30f6e190c2d76bb216bbad59e430254107551
Vara Bonthu <vara.bonthu@gmail.com>
2023-03-03 13:50:14 +0000

    Karpenter version2 emr on eks example

--- analytics/terraform/emr-eks-karpenter/variables.tf
+++ analytics/terraform/emr-eks-karpenter/variables.tf
@@ -1,6 +1,6 @@
 variable "name" {
   description = "Name of the VPC and EKS Cluster"
-  default     = "v5-emr-eks-karpenter"
+  default     = "v2-emr-eks-karpenter"
   type        = string
 }
 
--- analytics/terraform/emr-eks-yunikorn/variables.tf
+++ analytics/terraform/emr-eks-yunikorn/variables.tf
@@ -1,6 +1,6 @@
 variable "name" {
   description = "Name of the VPC and EKS Cluster"
-  default     = "emr-eks-yunikorn"
+  default     = "v2-emr-eks-yunikorn"
   type        = string
 }
 
