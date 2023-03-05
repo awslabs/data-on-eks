a0cbebbd06f8a2197d171a2fd8abdf091940be8e
Vara Bonthu <vara.bonthu@gmail.com>
2023-03-05 14:11:20 +0000

    Updated new precommit version file

--- schedulers/terraform/argo-workflow/data.tf
+++ schedulers/terraform/argo-workflow/data.tf
@@ -7,7 +7,3 @@ data "aws_availability_zones" "available" {}
 data "aws_caller_identity" "current" {}
 
 data "aws_partition" "current" {}
-
-data "aws_eks_cluster" "eks_cluster" {
-  name = module.eks_blueprints.eks_cluster_id
-}
