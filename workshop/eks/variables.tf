7697338ce6e33ac3ab603b2174c4b7c0a3fba8a6
Vara Bonthu <vara.bonthu@gmail.com>
2023-03-06 11:51:47 +0000

    Workshop first commit

---
+++ workshop/eks/variables.tf
@@ -0,0 +1,29 @@
+variable "name" {
+  description = "Name of the VPC and EKS Cluster"
+  default     = "doeks-workshop"
+  type        = string
+}
+
+variable "region" {
+  description = "region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "eks_cluster_version" {
+  description = "EKS Cluster version"
+  default     = "1.24"
+  type        = string
+}
+
+variable "tags" {
+  description = "Default tags"
+  default     = {}
+  type        = map(string)
+}
+
+variable "enable_yunikorn" {
+  default     = false
+  description = "Enable Apache YuniKorn Scheduler"
+  type        = bool
+}
