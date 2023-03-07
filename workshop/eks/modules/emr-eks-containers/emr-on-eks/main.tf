d7137bf929a04082d6e281d6ccad088857289b62
Vara Bonthu <vara.bonthu@gmail.com>
2023-03-07 10:41:33 +0000

    removed label

--- workshop/eks/modules/emr-eks-containers/emr-on-eks/main.tf
+++ workshop/eks/modules/emr-eks-containers/emr-on-eks/main.tf
@@ -18,9 +18,6 @@ locals {
 resource "kubernetes_namespace_v1" "this" {
   count = var.create_namespace ? 1 : 0
 
-  labels = {
-    job-type = "spark"
-  }
   metadata {
     name = var.namespace
   }
