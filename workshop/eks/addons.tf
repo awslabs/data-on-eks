13fac662bf7619c8f3037e05bae6a7bc53a7dc54
Vara Bonthu <vara.bonthu@gmail.com>
2023-03-14 12:46:36 +0000

    karpenter fix added

--- workshop/eks/addons.tf
+++ workshop/eks/addons.tf
@@ -143,20 +143,20 @@ resource "aws_prometheus_workspace" "amp" {
 #---------------------------------------
 # Karpenter Provisioners
 #---------------------------------------
-#data "kubectl_path_documents" "karpenter_provisioners" {
-#  pattern = "${path.module}/provisioners/spark-*.yaml"
-#  vars = {
-#    azs            = local.region
-#    eks_cluster_id = module.eks.cluster_name
-#  }
-#}
-#
-#resource "kubectl_manifest" "karpenter_provisioner" {
-#  for_each  = toset(data.kubectl_path_documents.karpenter_provisioners.documents)
-#  yaml_body = each.value
-#
-#  depends_on = [module.eks_blueprints_kubernetes_addons]
-#}
+data "kubectl_path_documents" "karpenter_provisioners" {
+  pattern = "${path.module}/provisioners/spark-*.yaml"
+  vars = {
+    azs            = local.region
+    eks_cluster_id = module.eks.cluster_name
+  }
+}
+
+resource "kubectl_manifest" "karpenter_provisioner" {
+  for_each  = toset(data.kubectl_path_documents.karpenter_provisioners.documents)
+  yaml_body = each.value
+
+  depends_on = [module.eks_blueprints_kubernetes_addons]
+}
 
 #---------------------------------------------------------------
 # Grafana Admin credentials resources
