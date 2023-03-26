80fbf4c94c4099eda204aa1eafd020b9a72083df
JIHED MSELMI <jihed.mselmi@gmail.com>
2023-03-26 15:18:58 +0200

    monitoring fix

--- distributed-databases/cloudnative-postgres/addons.tf
+++ distributed-databases/cloudnative-postgres/addons.tf
@@ -28,23 +28,24 @@ module "eks_blueprints_kubernetes_addons" {
   }
   enable_kube_prometheus_stack         = true
   kube_prometheus_stack_helm_config = {
-    name      = "kube-prometheus-stack"
-    chart     = "kube-prometheus-stack"
     namespace = "monitoring"
     values = [
       file("${path.module}/monitoring/kube-stack-config.yaml")
     ]
-    description = "kube-prometheus-stack helm Chart deployment configuration"
-    repository  = "https://prometheus-community.github.io/helm-charts"
-    version     = "45.6.0"
   }
-
-
   tags = local.tags
 }
 
 resource "kubectl_manifest" "cnpg_prometheus_rule" {
   yaml_body = file("${path.module}/monitoring/cnpg-prometheusrule.yaml")
+
+  depends_on = [
+    module.eks_blueprints_kubernetes_addons.kube_prometheus_stack
+  ]
+}
+
+resource "kubectl_manifest" "cnpg_grafana_cm" {
+  yaml_body = file("${path.module}/monitoring/grafana-configmap.yaml")
 
   depends_on = [
     module.eks_blueprints_kubernetes_addons.kube_prometheus_stack
