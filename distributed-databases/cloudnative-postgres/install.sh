ec2e6a3cd9f5ef96ff63ecf8eceb1bf256b8f121
Manabu Mccloskey <manabu.mccloskey@gmail.com>
2023-03-29 10:40:26 -0700

    initialize terraform in install scripts

--- distributed-databases/cloudnative-postgres/install.sh
+++ distributed-databases/cloudnative-postgres/install.sh
@@ -3,6 +3,9 @@
 read -p "Enter the region: " region
 export AWS_DEFAULT_REGION=$region
 
+echo "Initializing ..."
+terraform init || echo "\"terraform init\" failed"
+
 # List of Terraform modules to apply in sequence
 targets=(
   "module.vpc"
