fc95bc180cefcd1a6b213feecd91ff2a6d3e798e
Apoorva Kulkarni <kuapoorv@amazon.com>
2023-05-31 10:24:31 -0700

    fix for bugs

--- ai-ml/ray/terraform/install.sh
+++ ai-ml/ray/terraform/install.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
+set -o errexit
+set -o pipefail
 
-echo "Initializing ..."
-terraform init || echo "\"terraform init\" failed"
+targets=(
+  "module.vpc"
+  "module.karpenter_policy"
+  "module.memory_db"
+  "module.eks"
+  "module.eks_blueprints_addons"
+  "module.eks"
+)
 
+for target in "${targets[@]}"
+do
+  apply_output=$(terraform apply -target="$target" -auto-approve)
+  if [[ $? -eq 0 && $apply_output == *"Apply complete!"* ]]; then
+    echo "SUCCESS: Terraform apply of $target completed successfully"
+  else
+    echo "FAILED: Terraform apply of $target failed"
+    exit 1
+  fi
+done
 
-echo "Applying ..."
-terraform apply -auto-approve
-apply_output=$(terraform apply -auto-approve 2>&1)
-if [[ $? -eq 0 && $apply_output == *"Apply complete"* ]]; then
-  echo "SUCCESS: Terraform apply completed successfully"
+apply_output=$(terraform apply -auto-approve)
+if [[ $? -eq 0 && $apply_output == *"Apply complete!"* ]]; then
+  echo "SUCCESS: Terraform apply of all targets completed successfully"
 else
-  echo "FAILED: Terraform apply failed"
+  echo "FAILED: Terraform apply of all targets failed"
   exit 1
 fi
