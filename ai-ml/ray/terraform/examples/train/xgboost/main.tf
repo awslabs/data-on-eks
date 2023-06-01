c2a8c061d1c5104e2c71783773876464bbffe7aa
Apoorva Kulkarni <kuapoorv@amazon.com>
2023-06-01 09:11:55 -0700

    fix for kubernetes provider

--- ai-ml/ray/terraform/examples/train/xgboost/main.tf
+++ ai-ml/ray/terraform/examples/train/xgboost/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.example.certificate_authority[0].data)
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
