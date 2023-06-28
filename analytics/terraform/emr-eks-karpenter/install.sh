b317499e6eeab9d3c05f4775aae52521816a4930
Ray Krueger <raykrueger@gmail.com>
2023-06-28 19:40:45 +0000

    Reduce the number of times we run apply
    The current implementation of the install scripts runs `terraform apply`
    2 times per terraform module using -target. Additionally apply is run
      again 2 more times at the end of the script.
    
    This means that we are running terraform apply twice as much as
    required.
    
    This commit adds ` | tee /dev/tty` to all the apply steps so that the
    output can be captured and evaluated, as well as having the output print
    to screen.

--- analytics/terraform/emr-eks-karpenter/install.sh
+++ analytics/terraform/emr-eks-karpenter/install.sh
@@ -21,9 +21,8 @@ targets=(
 for target in "${targets[@]}"
 do
   echo "Applying module $target..."
-  terraform apply -target="$target" -auto-approve
-  apply_output=$(terraform apply -target="$target" -auto-approve 2>&1)
-  if [[ $? -eq 0 && $apply_output == *"Apply complete"* ]]; then
+  apply_output=$(terraform apply -target="$target" -auto-approve 2>&1 | tee /dev/tty)
+  if [[ ${PIPESTATUS[0]} -eq 0 && $apply_output == *"Apply complete"* ]]; then
     echo "SUCCESS: Terraform apply of $target completed successfully"
   else
     echo "FAILED: Terraform apply of $target failed"
@@ -33,9 +32,8 @@ done
 
 # Final apply to catch any remaining resources
 echo "Applying remaining resources..."
-terraform apply -auto-approve
-apply_output=$(terraform apply -auto-approve 2>&1)
-if [[ $? -eq 0 && $apply_output == *"Apply complete"* ]]; then
+apply_output=$(terraform apply -auto-approve 2>&1 | tee /dev/tty)
+if [[ ${PIPESTATUS[0]} -eq 0 && $apply_output == *"Apply complete"* ]]; then
   echo "SUCCESS: Terraform apply of all modules completed successfully"
 else
   echo "FAILED: Terraform apply of all modules failed"
