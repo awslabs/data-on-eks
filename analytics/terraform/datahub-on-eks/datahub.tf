c588af14616fc41ab812c7961ff78ce7d60a7c8f
Ovidiu Valeanu <ovaleanu@amazon.co.uk>
2023-07-19 10:10:35 +0100

    Fix comment about eks_blueprints_kubernetes_addons

--- analytics/terraform/datahub-on-eks/datahub.tf
+++ analytics/terraform/datahub-on-eks/datahub.tf
@@ -1,5 +1,5 @@
 module "datahub" {
-  depends_on          = [module.eks, module.eks_blueprints_kubernetes_addons]
+  depends_on          = [module.eks, module.eks_blueprints_addons]
   source              = "./datahub-addon"
   prefix              = local.name
   vpc_id              = module.vpc.vpc_id
