490f78fcec39c5bf3e2c476ed6b7b16d648e52de
Melody <meloyang@amazon.com>
2023-07-24 04:10:07 +0530

    fix region in variable

--- analytics/terraform/emr-eks-karpenter/variables.tf
+++ analytics/terraform/emr-eks-karpenter/variables.tf
@@ -7,7 +7,7 @@ variable "name" {
 variable "region" {
   description = "region"
   type        = string
-  default     = "us-west-2"
+  default     = "us-east-1"
 }
 
 variable "eks_cluster_version" {
