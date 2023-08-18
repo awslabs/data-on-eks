d4a0c93bdbe8cb8dd36fef5f6111a244c78c6f39
Vara Bonthu <vara.bonthu@gmail.com>
2023-08-18 20:05:01 +0100

    feat: Website updates for collapsible content

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {{onBrokenLinks: string, organizationName: string, plugins: string[], title: string, url: string, onBrokenMarkdownLinks: string, i18n: {defaultLocale: string, locales: string[]}, trailingSlash: boolean, baseUrl: string, presets: [string,Options][], githubHost: string, tagline: string, themeConfig: ThemeConfig & UserThemeConfig & AlgoliaThemeConfig, projectName: string}} */
 const config = {
   title: 'Data on EKS',
-  tagline: 'Supercharge your Data and Machine Learning Journey with Amazon EKS! 🚀',
+  tagline: 'Supercharge your Data and AI/ML Journey with Amazon EKS',
   url: 'https://awslabs.github.io',
   baseUrl: '/data-on-eks/',
   trailingSlash: false,
