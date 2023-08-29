4426e795ce97ffb9bfa8a814c287f6b63d73e5c6
Apoorva Kulkarni <kuapoorv@amazon.com>
2023-08-28 20:07:23 -0700

    add destroy.sh

---
+++ ai-ml/jark/terraform/destroy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "Destroying ..."
+
+# List of Terraform modules to apply in sequence
+targets=(  
+  "module.data_addons"
+  "module.eks_blueprints_addons"
+  "module.eks"
+  "module.vpc"
+)
+
+# Destroy modules in sequence
+for target in "${targets[@]}"
+do
+  echo "Destroying module $target..."
+  destroy_output=$(terraform destroy -target="$target" -auto-approve 2>&1 | tee /dev/tty)
+  if [[ ${PIPESTATUS[0]} -eq 0 && $destroy_output == *"Destroy complete"* ]]; then
+    echo "SUCCESS: Terraform destroy of $target completed successfully"
+  else
+    echo "FAILED: Terraform destroy of $target failed"
+    exit 1
+  fi
+done
+
+## Final destroy to catch any remaining resources
+echo "Destroying remaining resources..."
+destroy_output=$(terraform destroy -auto-approve 2>&1 | tee /dev/tty)
+if [[ ${PIPESTATUS[0]} -eq 0 && $destroy_output == *"Destroy complete"* ]]; then
+  echo "SUCCESS: Terraform destroy of all modules completed successfully"
+else
+  echo "FAILED: Terraform destroy of all modules failed"
+  exit 1
+fi
