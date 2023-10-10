f6cca7d856e1b94b48a9186a8cb2280a4a219237
jakeskyaws <116099058+jakeskyaws@users.noreply.github.com>
2023-10-10 15:43:32 +0100

    Update install.sh
    default region not being taken from env vars. Default variable taking presidence.

--- streaming/kafka/install.sh
+++ streaming/kafka/install.sh
@@ -16,7 +16,7 @@ targets=(
 for target in "${targets[@]}"
 do
   echo "Applying module $target..."
-  apply_output=$(terraform apply -target="$target" -auto-approve 2>&1 | tee /dev/tty)
+  apply_output=$(terraform apply -target="$target" -var=region=$(aws configure get region) -auto-approve 2>&1 | tee /dev/tty)
   if [[ ${PIPESTATUS[0]} -eq 0 && $apply_output == *"Apply complete"* ]]; then
     echo "SUCCESS: Terraform apply of $target completed successfully"
   else
@@ -27,7 +27,7 @@ done
 
 # Final apply to catch any remaining resources
 echo "Applying remaining resources..."
-apply_output=$(terraform apply -auto-approve 2>&1 | tee /dev/tty)
+apply_output=$(terraform apply -var=region=$(aws configure get region) -auto-approve 2>&1 | tee /dev/tty)
 if [[ ${PIPESTATUS[0]} -eq 0 && $apply_output == *"Apply complete"* ]]; then
   echo "SUCCESS: Terraform apply of all modules completed successfully"
 else
