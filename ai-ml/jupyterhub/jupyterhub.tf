2992953501f20294fc22a745df0ee0ebcf735ecc
Doruk Ozturk <dorukozturk@users.noreply.github.com>
2023-10-17 00:32:58 -0400

    Add cognito identity pool

--- ai-ml/jupyterhub/jupyterhub.tf
+++ ai-ml/jupyterhub/jupyterhub.tf
@@ -179,3 +179,17 @@ resource "aws_cognito_user_pool_client" "user_pool_client" {
 
   depends_on = [aws_cognito_user_pool_domain.domain]
 }
+
+resource "aws_cognito_identity_pool" "identity_pool" {
+
+  count = var.jupyter_hub_auth_mechanism == "cognito" ? 1 : 0
+  identity_pool_name = "jupyterhub-identity-pool"
+  allow_unauthenticated_identities = false  
+  cognito_identity_providers {
+    client_id = aws_cognito_user_pool_client.user_pool_client[0].id
+    provider_name = aws_cognito_user_pool.pool[0].endpoint
+    server_side_token_check = true
+  }
+
+  depends_on = [aws_cognito_user_pool_client.user_pool_client]
+}
