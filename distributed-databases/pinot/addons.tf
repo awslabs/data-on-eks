807bdbd81c81dd6517e4b82b02c03baf5e147d24
Wahab Syed <wahasyed@amazon.com>
2023-10-19 18:48:48 -0700

    feat: Adding prometheus scrap config

--- distributed-databases/pinot/addons.tf
+++ distributed-databases/pinot/addons.tf
@@ -16,6 +16,45 @@ module "ebs_csi_driver_irsa" {
 }
 
 #---------------------------------------------------------------
+# GP3 Encrypted Storage Class
+#---------------------------------------------------------------
+resource "kubernetes_annotations" "gp2_default" {
+  annotations = {
+    "storageclass.kubernetes.io/is-default-class" : "false"
+  }
+  api_version = "storage.k8s.io/v1"
+  kind        = "StorageClass"
+  metadata {
+    name = "gp2"
+  }
+  force = true
+
+  depends_on = [module.eks]
+}
+
+resource "kubernetes_storage_class" "ebs_csi_encrypted_gp3_storage_class" {
+  metadata {
+    name = "gp3"
+    annotations = {
+      "storageclass.kubernetes.io/is-default-class" : "true"
+    }
+  }
+
+  storage_provisioner    = "ebs.csi.aws.com"
+  reclaim_policy         = "Delete"
+  allow_volume_expansion = true
+  volume_binding_mode    = "WaitForFirstConsumer"
+  parameters = {
+    fsType    = "xfs"
+    encrypted = true
+    type      = "gp3"
+  }
+
+  depends_on = [kubernetes_annotations.gp2_default]
+}
+
+
+#---------------------------------------------------------------
 # IRSA for VPC CNI
 #---------------------------------------------------------------
 module "vpc_cni_irsa" {
@@ -77,6 +116,12 @@ module "eks_blueprints_kubernetes_addons" {
         name  = "grafana.adminPassword"
         value = data.aws_secretsmanager_secret_version.admin_password_version.secret_string
     }]
+
+    values = [
+      templatefile("${path.module}/helm/kube-prometheus-stack-values.yaml", {
+        storage_class_type = kubernetes_storage_class.ebs_csi_encrypted_gp3_storage_class.id,
+      })
+    ]
   }
 
   #---------------------------------------
