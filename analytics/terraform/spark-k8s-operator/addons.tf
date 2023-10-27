7739b0c7d7e8eb944b4fab2710e578ca5bf3fd9b
Alan Tyson <alanty@amazon.com>
2023-10-27 09:08:48 -0700

    Add AmazonSSMManagedInstanceCore policy to Karpenter nodes

--- analytics/terraform/spark-k8s-operator/addons.tf
+++ analytics/terraform/spark-k8s-operator/addons.tf
@@ -88,7 +88,11 @@ module "eks_blueprints_addons" {
     repository_username = data.aws_ecrpublic_authorization_token.token.user_name
     repository_password = data.aws_ecrpublic_authorization_token.token.password
   }
-
+  karpenter_node = {
+    iam_role_additional_policies = {
+      AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+    }
+  }
   #---------------------------------------
   # CloudWatch metrics for EKS
   #---------------------------------------
