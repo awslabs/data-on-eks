006321d4c038271a8ee9f693c5c84a1cecd4aa82
Sanjeev Ganjihal <sanjeevrg7@gmail.com>
2024-01-31 12:04:45 -0500

    AZ mapping Code changes (#421)
    Co-authored-by: Scott Perry <48838323+5cp@users.noreply.github.com>

--- ai-ml/trainium-inferentia/main.tf
+++ ai-ml/trainium-inferentia/main.tf
@@ -41,9 +41,70 @@ data "aws_ecrpublic_authorization_token" "token" {
 locals {
   name   = var.name
   region = var.region
-  # Training and Inference instances are available in the following AZs us-east-1 and us-west-2
-  # You can find the list of supported AZs here: https://aws.amazon.com/ec2/instance-types/trn1/
-  azs = ["${local.region}c", "${local.region}d"]
+  # Trn1 and Inf2 instances are available in specific AZs in us-east-1,
+  # us-east-2, and us-west-2. For Trn1, the first AZ id (below) should be used.
+  az_mapping = {
+    "us-west-2" = ["usw2-az4", "usw2-az1"],
+    "us-east-1" = ["use1-az6", "use1-az5"],
+    "us-east-2" = ["use2-az3", "use2-az1"]
+  }
+  azs = local.az_mapping[var.region]
+  tags = {
+    Blueprint  = local.name
+    GithubRepo = "github.com/awslabs/data-on-eks"
+  }
+}
+provider "aws" {
+  region = local.region
+}
+
+# ECR always authenticates with `us-east-1` region
+# Docs -> https://docs.aws.amazon.com/AmazonECR/latest/public/public-registries.html
+provider "aws" {
+  alias  = "ecr"
+  region = "us-east-1"
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.this.token
+  }
+}
+provider "kubectl" {
+  apply_retry_count      = 30
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+  load_config_file       = false
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
+
+data "aws_ecrpublic_authorization_token" "token" {
+  provider = aws.ecr
+}
+
+locals {
+  name   = var.name
+  region = var.region
+  # Trn1 and Inf2 instances are available in specific AZs in us-east-1,
+  # us-east-2, and us-west-2. For Trn1, the first AZ id (below) should be used.
+  az_mapping = {
+    "us-west-2" = ["usw2-az4", "usw2-az1"],
+    "us-east-1" = ["use1-az6", "use1-az5"],
+    "us-east-2" = ["use2-az3", "use2-az1"]
+  }
+  azs = local.az_mapping[var.region]
   tags = {
     Blueprint  = local.name
     GithubRepo = "github.com/awslabs/data-on-eks"
