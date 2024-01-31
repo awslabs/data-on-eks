aad1668940f85b1a52ff463a9d6c400e54670118
lusoal <lucasdu@amazon.com>
2024-01-31 14:01:08 -0600

    Removed duplicates from file

--- ai-ml/trainium-inferentia/main.tf
+++ ai-ml/trainium-inferentia/main.tf
@@ -2,8 +2,6 @@ provider "aws" {
   region = local.region
 }
 
-# ECR always authenticates with `us-east-1` region
-# Docs -> https://docs.aws.amazon.com/AmazonECR/latest/public/public-registries.html
 provider "aws" {
   alias  = "ecr"
   region = "us-east-1"
@@ -22,6 +20,7 @@ provider "helm" {
     token                  = data.aws_eks_cluster_auth.this.token
   }
 }
+
 provider "kubectl" {
   apply_retry_count      = 30
   host                   = module.eks.cluster_endpoint
@@ -41,8 +40,6 @@ data "aws_ecrpublic_authorization_token" "token" {
 locals {
   name   = var.name
   region = var.region
-  # Trn1 and Inf2 instances are available in specific AZs in us-east-1,
-  # us-east-2, and us-west-2. For Trn1, the first AZ id (below) should be used.
   az_mapping = {
     "us-west-2" = ["usw2-az4", "usw2-az1"],
     "us-east-1" = ["use1-az6", "use1-az5"],
@@ -53,43 +50,4 @@ locals {
     Blueprint  = local.name
     GithubRepo = "github.com/awslabs/data-on-eks"
   }
-}
-provider "aws" {
-  region = local.region
-}
-
-# ECR always authenticates with `us-east-1` region
-# Docs -> https://docs.aws.amazon.com/AmazonECR/latest/public/public-registries.html
-provider "aws" {
-  alias  = "ecr"
-  region = "us-east-1"
-}
-
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.this.token
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.this.token
-  }
-}
-provider "kubectl" {
-  apply_retry_count      = 30
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.this.token
-  load_config_file       = false
-}
-
-data "aws_eks_cluster_auth" "this" {
-  name = module.eks.cluster_name
-}
-
-data "aws_ecrpublic_authorization_token" "token" {
-  provider = aws.ecr
 }
