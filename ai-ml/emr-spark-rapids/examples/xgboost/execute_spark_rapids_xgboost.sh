aa542cdc3045e8baa9cd4ab10ddb351239b13e04
Vara Bonthu <vara.bonthu@gmail.com>
2024-02-19 09:47:36 -0800

    Updtes to the example shell script

--- ai-ml/emr-spark-rapids/examples/xgboost/execute_spark_rapids_xgboost.sh
+++ ai-ml/emr-spark-rapids/examples/xgboost/execute_spark_rapids_xgboost.sh
@@ -4,22 +4,37 @@
 #       ${S3_BUCKET}/${EMR_VIRTUAL_CLUSTER_ID}/${JOB_NAME}/input/fannie-mae-single-family-loan-performance/
 #       Follow these instructions to download the input data -> https://github.com/NVIDIA/spark-rapids-examples/blob/branch-23.04/docs/get-started/xgboost-examples/dataset/mortgage.md
 #--------------------------------------------
-read -p "Did you copy the Fannie Mae Loan Performance data to S3 bucket (y/n): "
-read -p "Enter the customized Docker image URI: " XGBOOST_IMAGE
-read -p "Enter EMR Virtual Cluster AWS Region: " AWS_REGION
-
-read -p "Enter the EMR Virtual Cluster ID: " EMR_VIRTUAL_CLUSTER_ID
-read -p "Enter the EMR Execution Role ARN: " EMR_EXECUTION_ROLE_ARN
-read -p "Enter the CloudWatch Log Group name: " CLOUDWATCH_LOG_GROUP
-
-read -p "Enter the S3 Bucket for storing PySpark Scripts, Pod Templates, Input data and Output data. e.g., test-bucket-123 : " S3_BUCKET
-read -p "Enter the number of executor instances (e.g., 8): " NUM_WORKERS
 #--------------------------------------------
 # DEFAULT VARIABLES CAN BE MODIFIED
 #--------------------------------------------
 JOB_NAME='spark-rapids-emr'
 EMR_EKS_RELEASE_LABEL="emr-7.0.0-spark-rapids-latest"
-#XGBOOST_IMAGE="public.ecr.aws/data-on-eks/emr-7.0.0-spark-rapids-xgboost-custom:latest"
+AWS_REGION="${AWS_REGION:-us-west-2}"  # Example default region
+XGBOOST_IMAGE="${XGBOOST_IMAGE:-public.ecr.aws/data-on-eks/emr-7.0.0-spark-rapids-xgboost-custom:latest}"
+NUM_WORKERS="${NUM_WORKERS:-8}"  # Example default number of executors
+
+read -p "Did you copy the Fannie Mae Loan Performance data to S3 bucket (y/n): "
+if [[ $REPLY != "y" ]]; then
+  echo "Please copy the inut data set to S3 before proceeding."
+  exit 1
+fi
+
+if [[ -z "$XGBOOST_IMAGE" ]]; then
+  read -p "Enter the customized Docker image URI (leave blank for default): " XGBOOST_IMAGE
+fi
+
+if [[ -z "$AWS_REGION" ]]; then
+  read -p "Enter EMR Virtual Cluster AWS Region (leave blank for default): " AWS_REGION
+fi
+
+if [[ -z "$NUM_WORKERS" ]]; then
+  read -p "Enter number of Spark executors (leave blank for default): " NUM_WORKERS
+fi
+
+read -p "Enter the EMR Virtual Cluster ID: (Check Terraform ouput values): " EMR_VIRTUAL_CLUSTER_ID
+read -p "Enter the EMR Execution Role ARN: (Check Terraform ouput values) " EMR_EXECUTION_ROLE_ARN
+read -p "Enter the CloudWatch Log Group name: (Check Terraform ouput values) " CLOUDWATCH_LOG_GROUP
+read -p "Enter the S3 Bucket for storing PySpark Scripts, Pod Templates, Input data and Output data. e.g., test-bucket-123 : " S3_BUCKET
 
 S3_BUCKET="s3://${S3_BUCKET}"
 
@@ -34,18 +49,6 @@ JARS_PATH="${SCRIPTS_S3_PATH}/jars"
 # Copy PySpark Scripts, Pod Templates and Input data to S3 bucket
 #--------------------------------------------
 aws s3 sync "./" ${SCRIPTS_S3_PATH}
-
-#--------------------------------------------
-# This job config is tuned for 8 node cluster with 1 GPU per node for g5.2xlarge instance type
-#--------------------------------------------
-# Tuning Best practices
-# https://nvidia.github.io/spark-rapids/docs/tuning-best-practices.html
-# spark.rapids.sql.batchSizeBytes= min(2GiB - 1 byte, ((gpu_memory - 1 GiB) / gpu_concurrency) / 4)
-# spark.sql.shuffle.partitions":"64" -> Number of executors(8) * 8 = 64
-# spark.rapids.memory.gpu.allocFraction=0.6
-# spark.rapids.memory.pinnedPool.size=2G
-# spark.rapids.sql.concurrentGpuTasks=2
-#--------------------------------------------
 
 #--------------------------------------------
 # Execute Spark job
@@ -121,7 +124,3 @@ aws emr-containers start-job-run \
       }
     }
   }'
-
-
-# Delete local JARS directory
-rm -rf ./jars
