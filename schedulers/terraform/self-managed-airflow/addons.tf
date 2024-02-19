78fd46c73094ca18e7b27c3a5d7b1362336912a2
Jagdeep <jphoolku@amazon.com>
2024-02-19 22:52:03 +0800

    Karpenter v0.34.0

--- schedulers/terraform/self-managed-airflow/addons.tf
+++ schedulers/terraform/self-managed-airflow/addons.tf
@@ -85,6 +85,7 @@ module "eks_blueprints_addons" {
     }
   }
   karpenter = {
+    chart_version       = "v0.34.0"
     repository_username = data.aws_ecrpublic_authorization_token.token.user_name
     repository_password = data.aws_ecrpublic_authorization_token.token.password
   }
