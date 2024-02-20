4a6dc588a0d3ea9062e037c6eb0c16c81e8b3cac
Jagdeep <jphoolku@amazon.com>
2024-02-20 16:29:34 +0800

    RDS Latest Modules

--- schedulers/terraform/self-managed-airflow/airflow-core.tf
+++ schedulers/terraform/self-managed-airflow/airflow-core.tf
@@ -13,7 +13,7 @@ module "db" {
   engine_version       = "14"
   family               = "postgres14" # DB parameter group
   major_engine_version = "14"         # DB option group
-  instance_class       = "db.t4g.large"
+  instance_class       = "db.m6i.xlarge"
 
   allocated_storage     = 20
   max_allocated_storage = 100
