244a516cb14351feb10fc3c905d0ee9dffd284f6
Doruk Ozturk <dorukozturk@users.noreply.github.com>
2024-02-22 22:12:50 -0500

    Add vpc and eks cluster

---
+++ ai-ml/nemo/versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.47"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.10"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.4"
+    }
+  }
+}
