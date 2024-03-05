ef32c78e752126a9d7416ea77bde57aaea2c6c66
Ratnopam Chakrabarti <ratnopamc@yahoo.com>
2024-03-04 19:55:48 -0800

    Deploy Gradio App as a k8s deployment and service for stable diffusion

--- ai-ml/trainium-inferentia/examples/inference/ray-serve/stable-diffusion-inf2/gradio-ui/gradio-app-stablediffusion.py
+++ ai-ml/trainium-inferentia/examples/inference/ray-serve/stable-diffusion-inf2/gradio-ui/gradio-app-stablediffusion.py
@@ -1,21 +1,19 @@
 import gradio as gr
 import requests
-import json
+import os
 from PIL import Image
 from io import BytesIO
 
 # Constants for model endpoint and service name
-model_endpoint = "/imagine"
-# service_name = "http://<REPLACE_ME_WITH_ELB_DNS_NAME>/serve"
-service_name = "http://localhost:8000"  # Replace with your actual service name
-
+model_endpoint = os.environ.get("MODEL_ENDPOINT", "/imagine")
+service_name = os.environ.get("SERVICE_NAME", "http://localhost:8000")
 
 # Function to generate image based on prompt
 def generate_image(prompt):
 
     # Create the URL for the inference
     url = f"{service_name}{model_endpoint}"
-
+    
     try:
         # Send the request to the model service
         response = requests.get(url, params={"prompt": prompt}, timeout=180)
@@ -25,9 +23,10 @@ def generate_image(prompt):
 
     except requests.exceptions.RequestException as e:
         # Handle any request exceptions (e.g., connection errors)
-        return f"AI: Error: {str(e)}"
+        # return f"AI: Error: {str(e)}"
+        return Image.new('RGB', (100, 100), color='red') 
 
 # Define the Gradio PromptInterface
 demo = gr.Interface(fn=generate_image,
                     inputs = [gr.Textbox(label="Enter the Prompt")],
-                    outputs = gr.Image(type='pil')).launch(debug='True')
+                    outputs = gr.Image(type='pil')).launch(server_name="0.0.0.0")
