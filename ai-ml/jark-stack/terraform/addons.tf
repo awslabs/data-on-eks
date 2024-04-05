7da6f3c1ff0dc6a31241173c0b9db82bf8085789
Vara Bonthu <vara.bonthu@gmail.com>
2024-04-05 11:46:18 -0700

    updpated nvidia deviceplugin version

--- ai-ml/jark-stack/terraform/addons.tf
+++ ai-ml/jark-stack/terraform/addons.tf
@@ -182,7 +182,7 @@ module "data_addons" {
   #---------------------------------------------------------------
   enable_nvidia_device_plugin = true
   nvidia_device_plugin_helm_config = {
-    version = "v0.15.0-rc.2"
+    version = "v0.14.5"
     name    = "nvidia-device-plugin"
     values = [
       <<-EOT
