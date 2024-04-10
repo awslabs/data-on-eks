3b24c8a15f8442e0e54daea608d4fb86f700f498
Ratnopam Chakrabarti <ratnopamc@yahoo.com>
2024-04-10 10:03:22 -0700

    add Dockerfile and ray-service manifest

--- ai-ml/trainium-inferentia/examples/inference/ray-serve/mistral-7B-inf2/README.md
+++ ai-ml/trainium-inferentia/examples/inference/ray-serve/mistral-7B-inf2/README.md
@@ -1,2 +1,4 @@
 # Run Inference using Mistral-7B on AWS Neuron(Inf2)
 
+Install multiple k8s-scheduler extension on EKS to work with AWS Neuron packages.
+
--- ai-ml/trainium-inferentia/examples/inference/ray-serve/mistral-7B-inf2/ray_serve_mistral.py
+++ ai-ml/trainium-inferentia/examples/inference/ray-serve/mistral-7B-inf2/ray_serve_mistral.py
@@ -2,50 +2,73 @@ from io import BytesIO
 from fastapi import FastAPI
 from fastapi.responses import Response
 import os
-import base64
+
 
 from ray import serve
+import torch
 
 app = FastAPI()
 
 neuron_cores = 2
 
-@serve.deployment(name="stable-diffusion-api", num_replicas=1, route_prefix="/")
+@serve.deployment(name="mistral-deployment", num_replicas=1, route_prefix="/")
 @serve.ingress(app)
 class APIIngress:
-    def __init__(self, diffusion_model_handle) -> None:
-        self.handle = diffusion_model_handle
+    def __init__(self, mistral_model_handle) -> None:
+        self.handle = mistral_model_handle
 
-    @app.get(
-        "/imagine",
-        responses={200: {"content": {"image/png": {}}}},
-        response_class=Response,
-    )
-    async def generate(self, prompt: str):
-        image = await self.handle.generate.remote(prompt)
-        file_stream = BytesIO()
-        image.save(file_stream, "PNG")
-        return Response(content=file_stream.getvalue(), media_type="image/png")
+    @app.get("/infer")
+    async def infer(self, sentence: str):
+        # Asynchronously perform inference using the provided sentence
+        ref = await self.handle.infer.remote(sentence)
+        # Await the result of the asynchronous inference and return it
+        result = await ref
+        return result
+    
 
-@serve.deployment(name="stable-diffusion-v2",
+@serve.deployment(name="mistral-7b",
     autoscaling_config={"min_replicas": 0, "max_replicas": 6},
     ray_actor_options={
         "resources": {"neuron_cores": neuron_cores},
         "runtime_env": {"env_vars": {"NEURON_CC_FLAGS": "-O1"}},
     },
 )
-class StableDiffusionV2:
+class MistralModel:
     def __init__(self):
-        from optimum.neuron import NeuronStableDiffusionXLPipeline
+
+        from transformers import AutoTokenizer
+        from transformers_neuronx import MistralForSampling, GQA, NeuronConfig
 
         model_id = os.getenv('MODEL_ID')
 
-        # To avoid saving the model locally, we can use the pre-compiled model directly from HF
-        self.pipe = NeuronStableDiffusionXLPipeline.from_pretrained(model_id, device_ids=[0, 1])
+        # Set sharding strategy for GQA to be shard over heads
+        neuron_config = NeuronConfig(
+            group_query_attention=GQA.SHARD_OVER_HEADS
+        )
 
-    async def generate(self, prompt: str):
-        assert len(prompt), "prompt parameter cannot be empty"
-        image = self.pipe(prompt).images[0]
-        return image
+        # Create and compile the Neuron model
+        self.neuron_model = MistralForSampling.from_pretrained(model_id, amp='bf16', neuron_config=neuron_config)
+        self.neuron_model.to_neuron()
 
-entrypoint = APIIngress.bind(StableDiffusionV2.bind())
+        # Get a tokenizer and exaple input
+        self.tokenizer = AutoTokenizer.from_pretrained(model_id)
+  
+
+        # Define the method for performing inference with the Mistral model
+    def infer(self, sentence: str):
+
+        text = "[INST] What is your favourite condiment? [/INST]"
+        sentence = text
+        # Tokenize the input sentence and encode it
+        encoded_input = self.tokenizer.encode(sentence, return_tensors='pt')
+
+        # Run inference
+        with torch.inference_mode():
+            generated_sequence = self.neuron_model.sample(encoded_input, sequence_length=256, start_ids=None)
+
+        # Decode the generated sequences and return the results
+        return [self.tokenizer.decode(seq, skip_special_tokens=True) for seq in generated_sequence]
+
+
+
+entrypoint = APIIngress.bind(MistralModel.bind())
