da3d101ad24bb769a1a1e84b1ea8b28f86ddeb87
avasu80 <vasuaswi@amazon.com>
2024-05-13 18:18:18 -0700

    made changes to the mark up page

--- analytics/terraform/superset-on-eks/variables.tf
+++ analytics/terraform/superset-on-eks/variables.tf
@@ -7,7 +7,7 @@ variable "name" {
 variable "region" {
   description = "Region"
   type        = string
-  default     = "us-east-1"
+  default     = "us-west-1"
 }
 
 variable "eks_cluster_version" {
