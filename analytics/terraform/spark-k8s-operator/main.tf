1590fd46c0191152ec3b1ee4f44f8aaa1796e097
Ovidiu Valeanu <ovaleanu@amazon.co.uk>
2024-07-03 20:32:52 +0100

    feat: spark on eks workshop updates

--- analytics/terraform/spark-k8s-operator/main.tf
+++ analytics/terraform/spark-k8s-operator/main.tf
@@ -58,6 +58,13 @@ data "aws_availability_zones" "available" {}
 data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
 data "aws_partition" "current" {}
+data "aws_iam_session_context" "current" {
+  # This data source provides information on the IAM source role of an STS assumed role
+  # For non-role ARNs, this data source simply passes the ARN through issuer ARN
+  # Ref https://github.com/terraform-aws-modules/terraform-aws-eks/issues/2327#issuecomment-1355581682
+  # Ref https://github.com/hashicorp/terraform-provider-aws/issues/28381
+  arn = data.aws_caller_identity.current.arn
+}
 
 #---------------------------------------------------------------
 # Example IAM policy for Spark job execution
