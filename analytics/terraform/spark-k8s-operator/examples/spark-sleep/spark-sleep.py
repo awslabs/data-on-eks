1899b525e2720f7ed671ac9634680698cdb6186c
Alan Tyson <alanty@amazon.com>
2024-08-06 09:21:15 -0700

    updated examples

---
+++ analytics/terraform/spark-k8s-operator/examples/spark-sleep/spark-sleep.py
@@ -0,0 +1,54 @@
+import logging
+import sys
+import time
+from datetime import datetime
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql import functions as f
+
+# Logging configuration
+formatter = logging.Formatter('[%(asctime)s] %(levelname)s @ line %(lineno)d: %(message)s')
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.INFO)
+handler.setFormatter(formatter)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
+
+dt_string = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+AppName = "sparkSleep"
+
+
+def main(args):
+
+    sleep_time = args[1]
+
+    # Create Spark Session
+    spark = SparkSession \
+        .builder \
+        .appName(AppName + "_" + str(dt_string)) \
+        .getOrCreate()
+
+    spark.sparkContext.setLogLevel("INFO")
+    logger.info("Starting spark application")
+
+    logger.info("Sleeping for %s seconds", sleep_time)
+    
+    time.sleep(sleep_time)
+
+    logger.info("Ending spark application")
+    
+    # end spark code
+    spark.stop()
+
+    return None
+
+
+if __name__ == "__main__":
+    print(len(sys.argv))
+    if len(sys.argv) != 3:
+        print("Usage: spark-etl [input-folder] [output-folder]")
+        sys.exit(0)
+
+    main(sys.argv)
