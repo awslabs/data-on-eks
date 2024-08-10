95d4e644182ef8a91555ed4b56ec2864b8f3b623
Ovidiu Valeanu <ovaleanu@amazon.co.uk>
2024-08-10 13:21:57 +0300

    spark on eks ipv6

--- analytics/terraform/spark-eks-ipv6/main.tf
+++ analytics/terraform/spark-eks-ipv6/main.tf
@@ -77,8 +77,6 @@ locals {
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
   account_id = data.aws_caller_identity.current.account_id
-  partition  = data.aws_partition.current.partition
-
   tags = {
     Blueprint  = local.name
     GithubRepo = "github.com/awslabs/data-on-eks"
