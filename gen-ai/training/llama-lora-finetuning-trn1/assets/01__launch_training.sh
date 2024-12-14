52e643d6c2ee060990803229b78bf20586ef4528
Vijoy Choyi <81345550+VijoyChoyi@users.noreply.github.com>
2024-12-13 16:07:28 -0800

    initial code

---
+++ gen-ai/training/llama-lora-finetuning-trn1/assets/01__launch_training.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export HF_TOKEN=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       # Required for gated models like llama3-8b
+export BASE_DIR="/shared/finetuned_models"
+export OUTPUT_DIR=`date "+%Y%m%d_%H%M%S"`
+
+torchrun \
+    --nproc_per_node=32 \
+    finetune_llama.py \
+    --output_dir $BASE_DIR/$OUTPUT_DIR \
+    --tensor_parallel_size 8 \
+    --bf16 True \
+    --per_device_train_batch_size 8 \
+    --gradient_accumulation_steps 1 \
+    --gradient_checkpointing True \
+    --max_steps 250 \
+    --logging_steps 10 \
+    --learning_rate 1e-5 \
+    --dataloader_drop_last True \
+    # --do_eval True \
+    # --eval_strategy steps \
+    # --eval_steps 100 \
+    # --per_device_eval_batch_size 8
+
+echo "Output dir = $BASE_DIR/$OUTPUT_DIR"
---
+++ gen-ai/training/llama-lora-finetuning-trn1/assets/02__consolidate_adapter_shards_and_merge_model.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 ./02__consolidate_adapter_shards_and_merge_model.py -i <path_to_chkpt_dir> -o <new_model>
