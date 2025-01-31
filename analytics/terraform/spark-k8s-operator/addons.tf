c57303633f006cb4ef42380ef82896b802083e70
alanty <45295374+alanty@users.noreply.github.com>
2025-01-31 11:53:03 -0800

    feat: Enable spark-operator prometheus metrics  (#737)

--- analytics/terraform/spark-k8s-operator/addons.tf
+++ analytics/terraform/spark-k8s-operator/addons.tf
@@ -386,6 +386,24 @@ module "eks_data_addons" {
           rbac:
             # -- Specifies whether to create RBAC resources for the controller.
             create: false
+        prometheus:
+          metrics:
+            enable: true
+            port: 8080
+            portName: metrics
+            endpoint: /metrics
+            prefix: ""
+          # Prometheus pod monitor for controller pods
+          podMonitor:
+            # -- Specifies whether to create pod monitor.
+            create: true
+            labels: {}
+            # -- The label to use to retrieve the job name from
+            jobLabel: spark-operator-podmonitor
+            # -- Prometheus metrics endpoint properties. `metrics.portName` will be used as a port
+            podMetricsEndpoint:
+              scheme: http
+              interval: 5s
       EOT
     ]
   }
