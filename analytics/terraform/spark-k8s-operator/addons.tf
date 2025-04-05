96ed32e0e9fd3e3824a105c5d88aed6949fbdbeb
Ananth Raghavendra <raganant@amazon.com>
2025-04-04 17:11:11 -0700

    Adding Graviton nodepool for On-Demand and running Spark drivers on On-Demand instances

--- analytics/terraform/spark-k8s-operator/addons.tf
+++ analytics/terraform/spark-k8s-operator/addons.tf
@@ -521,6 +521,53 @@ module "eks_data_addons" {
       EOT
       ]
     }
+    spark-compute-graviton-on-demand = {
+      values = [
+        <<-EOT
+      name: spark-compute-graviton-on-demand
+      clusterName: ${module.eks.cluster_name}
+      ec2NodeClass:
+        karpenterRole: ${split("/", module.eks_blueprints_addons.karpenter.node_iam_role_arn)[1]}
+        subnetSelectorTerms:
+          tags:
+            Name: "${module.eks.cluster_name}-private*"
+        securityGroupSelectorTerms:
+          tags:
+            Name: ${module.eks.cluster_name}-node
+        instanceStorePolicy: RAID0
+      nodePool:
+        labels:
+          - type: karpenter
+          - NodeGroupType: SparkGravitonOnDemand
+          - multiArch: Spark
+        requirements:
+          - key: "karpenter.sh/capacity-type"
+            operator: In
+            values: ["on-demand"]
+          - key: "kubernetes.io/arch"
+            operator: In
+            values: ["arm64"]
+          - key: "karpenter.k8s.aws/instance-category"
+            operator: In
+            values: ["m", "c", "r"]
+          - key: "karpenter.k8s.aws/instance-cpu"
+            operator: In
+            values: ["4", "8", "16", "32"]
+          - key: "karpenter.k8s.aws/instance-hypervisor"
+            operator: In
+            values: ["nitro"]
+          - key: "karpenter.k8s.aws/instance-generation"
+            operator: Gt
+            values: ["2"]
+        limits:
+          cpu: 2000
+        disruption:
+          consolidationPolicy: WhenEmptyOrUnderutilized
+          consolidateAfter: 1m
+        weight: 100
+      EOT
+      ]
+    }
     spark-compute-graviton = {
       values = [
         <<-EOT
