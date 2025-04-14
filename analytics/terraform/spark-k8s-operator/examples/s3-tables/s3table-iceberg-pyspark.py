cfdad5f54b7e20cdfdf116924f24e5db516d4947
Manabu McCloskey <manabu.mccloskey@gmail.com>
2025-04-14 16:01:38 -0700

    fix: Update s3tables examples to use iceberg rest catalog api (#816)
    Signed-off-by: Manabu McCloskey <manabu.mccloskey@gmail.com>

--- analytics/terraform/spark-k8s-operator/examples/s3-tables/s3table-iceberg-pyspark.py
+++ analytics/terraform/spark-k8s-operator/examples/s3-tables/s3table-iceberg-pyspark.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+import os
 from datetime import datetime
 
 from pyspark.sql import SparkSession
@@ -17,6 +18,8 @@ logger.addHandler(handler)
 dt_string = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
 AppName = "EmployeeDataS3TableJob"
 
+# Get AWS information
+region = os.environ.get('AWS_REGION', 'us-east-1')
 
 def main(args):
     """
@@ -30,25 +33,33 @@ def main(args):
     input_csv_path = args[1]  # Path to the input CSV file
     s3table_arn = args[2]  # s3table arn
 
+    # Configuration parameters
+    namespace = "doeks_namespace"
+    catalog_name = "s3tablesbucket"
+    table_name = "employee_s3_table"
+    full_table_name = f"s3tablesbucket.{namespace}.{table_name}"
+    catalog_uri =  f"https://s3tables.{region}.amazonaws.com/iceberg"
+
     # Initialize Spark session
     logger.info("Initializing Spark Session")
     spark = (SparkSession
              .builder
              .appName(f"{AppName}_{dt_string}")
              .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
-             .config("spark.sql.catalog.s3tablesbucket", "org.apache.iceberg.spark.SparkCatalog")
-             .config("spark.sql.catalog.s3tablesbucket.catalog-impl", "software.amazon.s3tables.iceberg.S3TablesCatalog")
-             .config("spark.sql.catalog.s3tablesbucket.warehouse", s3table_arn)
-             .config('spark.hadoop.fs.s3.impl', "org.apache.hadoop.fs.s3a.S3AFileSystem")
+             .config(f"spark.sql.catalog.{catalog_name}", "org.apache.iceberg.spark.SparkCatalog")
+             .config(f"spark.sql.catalog.{catalog_name}.warehouse", s3table_arn)
+             .config(f"spark.sql.catalog.{catalog_name}.type", "rest")
+             .config(f"spark.sql.catalog.{catalog_name}.rest.sigv4-enabled", "true")
+             .config(f"spark.sql.catalog.{catalog_name}.rest.signing-name", "s3tables")
+             .config(f"spark.sql.catalog.{catalog_name}.rest.signing-region", region)
+             .config(f"spark.sql.catalog.{catalog_name}.uri", catalog_uri)
+             .config(f"spark.sql.catalog.{catalog_name}.io-impl", "org.apache.iceberg.aws.s3.S3FileIO")
+             .config("spark.hadoop.fs.s3.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
              .config("spark.sql.defaultCatalog", "s3tablesbucket")
              .getOrCreate())
 
     spark.sparkContext.setLogLevel("INFO")
     logger.info("Spark session initialized successfully")
-
-    namespace = "doeks_namespace"
-    table_name = "employee_s3_table"
-    full_table_name = f"s3tablesbucket.{namespace}.{table_name}"
 
     # Step 1: Create namespace if not exists
     logger.info(f"Creating namespace: {namespace}")
@@ -67,13 +78,26 @@ def main(args):
     logger.info("Source data count:")
     employee_df.count()
 
-    # Step 3: Create or replace table and write data in one operation
-    logger.info(f"Creating/Replacing and writing data to table: {full_table_name}")
-    (employee_df.writeTo(full_table_name)
-                .using("iceberg")
-                .createOrReplace())
+    # Step 3: Create a table for generated data
+    spark.sql(f"""
+        CREATE TABLE IF NOT EXISTS {full_table_name} (
+            id INT,
+            name STRING,
+            level STRING,
+            salary DOUBLE
+        )
+        USING iceberg
+        TBLPROPERTIES (
+            'format-version'='2',
+            'write.format.default'='parquet'
+        )
+    """)
 
-    # Step 4: Read data back from the Iceberg table
+    # Step 4: Write data to the table
+    logger.info(f"Creating/Replacing and writing data to table: {full_table_name}")
+    employee_df.writeTo(full_table_name).using('iceberg').append()
+
+    # Step 5: Read data back from the Iceberg table
     logger.info(f"Reading data back from Iceberg table: {full_table_name}")
     iceberg_data_df = spark.read.format("iceberg").load(full_table_name)
 
