b9494a74988e5efa121fed6418be29f31dfe8e30
Manabu McCloskey <manabu.mccloskey@gmail.com>
2025-06-18 17:32:32 -0700

    revert changes for streaming and distributed-databases
    Signed-off-by: Manabu McCloskey <manabu.mccloskey@gmail.com>

--- streaming/flink/versions.tf
+++ streaming/flink/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.95"
+      version = ">= 5.34"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
@@ -12,7 +12,7 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.17"
+      version = ">= 2.4.1"
     }
     kubectl = {
       source  = "alekc/kubectl"
