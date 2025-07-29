9d28c7b74ab0864919524b20a79cc25b9deb6a0f
e0100018 <fabrice.scellos@soprasteria.com>
2025-07-29 12:30:24 +0200

    Ajout GW Istio pour kubeflow

--- poc-outpost/modules/kubeflow/main.tf
+++ poc-outpost/modules/kubeflow/main.tf
@@ -1,3 +1,7 @@
+locals {
+
+}
+
 #---------------------------------------------------------------
 # Déploiement du ClusterIssuer Kubeflow
 #---------------------------------------------------------------
@@ -14,4 +18,24 @@ module "clusterissuer_kubeflow" {
     kustomization = kustomization
     helm = helm
   }
+}
+
+## Installation d'une Ingress GW dédiée à Kubeflow
+## (Pour simplifier l'intégration avec les softs hors Kubeflow, utilisation du même SA que celle déjà en place)
+## La GW est "kubeflow-ingressgateway"
+resource "helm_release" "kubeflowgw" {
+  name             = "kubeflow-istio-ingress"
+  namespace        = "istio-ingress"
+  create_namespace = false
+
+  repository        = "https://istio-release.storage.googleapis.com/charts"
+  chart             = "gateway"
+  version           = "1.26.2"
+  dependency_update = true
+  upgrade_install   = true
+  wait              = true
+
+  values = [
+    templatefile("${path.module}/helm-values/kubeflow-ingress-values.yaml", {})
+  ]
 }
