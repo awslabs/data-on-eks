4e5c60d8481524285e717834d7857bce1eb40273
Manabu McCloskey <manabu.mccloskey@gmail.com>
2025-08-11 16:27:08 -0700

    fix: Fix tf lint (#887)

--- analytics/terraform/spark-k8s-operator/jupyterhub.tf
+++ analytics/terraform/spark-k8s-operator/jupyterhub.tf
@@ -59,13 +59,13 @@ resource "kubernetes_secret_v1" "jupyterhub_single_user" {
 #---------------------------------------------------------------------
 data "aws_iam_policy_document" "bedrock_jupyter" {
   statement {
-    sid       = "BedrockModelAccess"
-    effect    = "Allow"
+    sid    = "BedrockModelAccess"
+    effect = "Allow"
     resources = [
       "arn:${data.aws_partition.current.partition}:bedrock:*::foundation-model/*",
       "arn:aws:bedrock:*:*:inference-profile/us.anthropic.claude-3-7-sonnet-20250219-v1:0",
       "arn:aws:bedrock:*:*:inference-profile/us.anthropic.claude-sonnet-4-20250514-v1:0"
-      ]
+    ]
     actions = [
       "bedrock:InvokeModel",
       "bedrock:InvokeModelWithResponseStream"
