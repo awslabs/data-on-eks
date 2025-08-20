6a9d6f66720ac8079436f599d84e82ebd31070c1
Sai Teja Penugonda <saipenug@amazon.com>
2025-08-20 14:57:36 -0400

    fixed karpenter node pool syntax issue

--- streaming/kafka/addons.tf
+++ streaming/kafka/addons.tf
@@ -280,12 +280,8 @@ module "eks_data_addons" {
         instanceStorePolicy: RAID0
       nodePool:
         labels:
-          type: karpenter
-          NodeGroupType: kafka
-        taints:
-          - key: dedicated
-            value: kafka
-            effect: NoSchedule
+          - type: karpenter
+          - NodeGroupType: kafka
         requirements:
           - key: "kubernetes.io/arch"
             operator: In
