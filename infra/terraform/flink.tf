3feb25abf32b3aa27f7cb8e9f8476a634952afc9
Manabu McCloskey <manabu.mccloskey@gmail.com>
2025-09-19 12:10:53 -0700

    update flink operator values
    Signed-off-by: Manabu McCloskey <manabu.mccloskey@gmail.com>

--- infra/terraform/flink.tf
+++ infra/terraform/flink.tf
@@ -1,6 +1,13 @@
+locals {
+  flink_operator_values = yamldecode(templatefile("${path.module}/helm-values/flink-operator.yaml", {
+  }))
+}
+
 resource "kubectl_manifest" "flink_operator" {
-    count = var.enable_flink ? 1 : 0
-  yaml_body = templatefile("${path.module}/argocd-applications/flink-operator.yaml", {})
+  count = var.enable_flink ? 1 : 0
+  yaml_body = templatefile("${path.module}/argocd-applications/flink-operator.yaml", {
+    user_values_yaml = indent(10, yamlencode(local.flink_operator_values))
+  })
 
   depends_on = [
     helm_release.argocd,
