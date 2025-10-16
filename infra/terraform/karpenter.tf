5b0b1dd287a4ad3b7967f886751ea9f47d51b9e4
Manabu McCloskey <manabu.mccloskey@gmail.com>
2025-10-16 23:16:59 +0000

    remove ecr token
    Signed-off-by: Manabu McCloskey <manabu.mccloskey@gmail.com>

--- infra/terraform/karpenter.tf
+++ infra/terraform/karpenter.tf
@@ -50,9 +50,6 @@ resource "helm_release" "karpenter" {
   version          = "1.6.1"
   wait             = true
 
-  repository_username = data.aws_ecrpublic_authorization_token.token.user_name
-  repository_password = data.aws_ecrpublic_authorization_token.token.password
-
   values = [
     <<-EOT
     nodeSelector:
