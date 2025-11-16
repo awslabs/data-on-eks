cd8bbb72428729da6ab5f5928d6f09ff11177177
alanty <45295374+alanty@users.noreply.github.com>
2025-11-16 08:55:37 -0800

    add Auto Mode resources

--- analytics/terraform/spark-k8s-operator/eks.tf
+++ analytics/terraform/spark-k8s-operator/eks.tf
@@ -14,17 +14,26 @@ module "eks" {
   # Add the IAM identity that terraform is using as a cluster admin
   authentication_mode                      = "API_AND_CONFIG_MAP"
   enable_cluster_creator_admin_permissions = true
+  enable_irsa = true
+
+  # Enable EKS Auto Mode
+  cluster_compute_config = {
+    enabled    = true
+    node_pools = ["system","general-purpose"]
+  }
 
   #---------------------------------------
   # Amazon EKS Managed Add-ons
   #---------------------------------------
   cluster_addons = {
-    coredns                = {}
-    eks-pod-identity-agent = {}
+    coredns    = {}
+    kube-proxy = {}
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
     vpc-cni = {
       before_compute = true
       preserve       = true
-      most_recent    = true # To ensure access to the latest settings provided
       configuration_values = jsonencode({
         env = {
           # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
@@ -127,80 +136,6 @@ module "eks" {
   }
 
   eks_managed_node_groups = {
-    #  We recommend to have a MNG to place your critical workloads and add-ons
-    #  Then rely on Karpenter to scale your workloads
-    #  You can also make uses on nodeSelector and Taints/tolerations to spread workloads on MNG or Karpenter provisioners
-    core_node_group = {
-      name        = "core-node-group"
-      description = "EKS managed node group example launch template"
-      # Filtering only Secondary CIDR private subnets starting with "100.". Subnet IDs where the nodes/node groups will be provisioned
-      subnet_ids = compact([for subnet_id, cidr_block in zipmap(module.vpc.private_subnets, module.vpc.private_subnets_cidr_blocks) :
-        substr(cidr_block, 0, 4) == "100." ? subnet_id : null]
-      )
-
-      min_size     = 3
-      max_size     = 3
-      desired_size = 3
-
-      instance_types = ["m5.xlarge"]
-
-      labels = {
-        WorkerType    = "ON_DEMAND"
-        NodeGroupType = "core"
-      }
-
-      tags = {
-        Name                     = "core-node-grp",
-        "karpenter.sh/discovery" = local.name
-      }
-    }
-
-    # The following Node groups are a placeholder to create Node groups for running Spark TPC-DS benchmarks
-    spark_benchmark_ebs = {
-      name        = "spark_benchmark_ebs"
-      description = "Managed node group for Spark Benchmarks with EBS using x86 or ARM"
-      # Filtering only Secondary CIDR private subnets starting with "100.". Subnet IDs where the nodes/node groups will be provisioned
-      subnet_ids = [element(compact([for subnet_id, cidr_block in zipmap(module.vpc.private_subnets, module.vpc.private_subnets_cidr_blocks) :
-        substr(cidr_block, 0, 4) == "100." ? subnet_id : null]), 0)
-      ]
-
-      # Change ami_type= AL2023_x86_64_STANDARD for x86 instances
-      ami_type = "AL2023_ARM_64_STANDARD" # arm64
-
-      # Node group will be created with zero instances when you deploy the blueprint.
-      # You can change the min_size and desired_size to 6 instances
-      # desired_size might not be applied through terrafrom once the node group is created so this needs to be adjusted in AWS Console.
-      min_size     = 0 # Change min and desired to 6 for running benchmarks
-      max_size     = 8
-      desired_size = 0 # Change min and desired to 6 for running benchmarks
-
-      # This storage is used as a shuffle for non NVMe SSD instances. e.g., r8g instances
-      block_device_mappings = {
-        xvda = {
-          device_name = "/dev/xvda"
-          ebs = {
-            volume_size           = 300
-            volume_type           = "gp3"
-            iops                  = 3000
-            encrypted             = true
-            delete_on_termination = true
-          }
-        }
-      }
-
-      # Change the instance type as you desire and match with ami_type
-      instance_types = ["r8g.12xlarge"] # Change Instance type to run the benchmark with various instance types
-
-      labels = {
-        NodeGroupType = "spark_benchmark_ebs"
-      }
-
-      tags = {
-        Name          = "spark_benchmark_ebs"
-        NodeGroupType = "spark_benchmark_ebs"
-      }
-    }
-
     spark_benchmark_ssd = {
       name        = "spark_benchmark_ssd"
       description = "Managed node group for Spark Benchmarks with NVMEe SSD using x86 or ARM"
