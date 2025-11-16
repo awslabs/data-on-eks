57d0218c124e0e24bfab16280a0719f3434b80c0
alanty <45295374+alanty@users.noreply.github.com>
2025-11-16 09:24:42 -0800

    pin IRSA module version

--- analytics/terraform/spark-k8s-operator/iam.tf
+++ analytics/terraform/spark-k8s-operator/iam.tf
@@ -4,7 +4,7 @@
 #---------------------------------------------------------------
 module "ebs_csi_driver_irsa" {
   source                = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version               = "~> 5.55"
+  version               = "5.60.0 "
   role_name_prefix      = format("%s-%s-", local.name, "ebs-csi-driver")
   attach_ebs_csi_policy = true
   oidc_providers = {
