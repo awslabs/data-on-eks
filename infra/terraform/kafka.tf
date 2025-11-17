80b1f689fcfeb59bf4b57d83ec92dbdb54fcb427
Manabu McCloskey <manabu.mccloskey@gmail.com>
2025-11-17 17:27:41 +0000

    remove kafka cluster creation
    Signed-off-by: Manabu McCloskey <manabu.mccloskey@gmail.com>

--- infra/terraform/kafka.tf
+++ infra/terraform/kafka.tf
@@ -1,6 +1,5 @@
 locals {
   strimzi_kafka_operator_values = templatefile("${path.module}/helm-values/strimzi-kafka-operator.yaml", {})
-  kafka_manifests_base_path     = "${path.root}/../data-stacks/kafka-on-eks/kafka-cluster"
 }
 
 resource "kubectl_manifest" "strimzi_kafka_operator" {
@@ -10,26 +9,5 @@ resource "kubectl_manifest" "strimzi_kafka_operator" {
 
   depends_on = [
     helm_release.argocd,
-  ]
-}
-
-#---------------------------------------------------------------
-# Kafka Namespace
-#---------------------------------------------------------------
-resource "kubectl_manifest" "kafka_namespace" {
-  yaml_body = templatefile("${local.kafka_manifests_base_path}/namespace.yaml", {})
-}
-
-#---------------------------------------------------------------
-# Kafka Manifests
-#---------------------------------------------------------------
-resource "kubectl_manifest" "kafka_manifests" {
-  for_each = fileset(local.kafka_manifests_base_path, "*.yaml")
-
-  yaml_body = templatefile("${local.kafka_manifests_base_path}/${each.value}", {})
-
-  depends_on = [
-    kubectl_manifest.strimzi_kafka_operator,
-    kubectl_manifest.kafka_namespace
   ]
 }
