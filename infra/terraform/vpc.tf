3bacff22918e78789a4fdd1286a560e9f18ed6e7
Manabu McCloskey <manabu.mccloskey@gmail.com>
2026-01-12 18:41:45 +0000

    use vpc endpoints for s3
    Signed-off-by: Manabu McCloskey <manabu.mccloskey@gmail.com>

--- infra/terraform/vpc.tf
+++ infra/terraform/vpc.tf
@@ -56,3 +56,70 @@ module "vpc" {
   })
 
 }
+
+#---------------------------------------------------------------
+# VPC Endpoints
+#---------------------------------------------------------------
+resource "aws_security_group" "vpc_endpoint_s3" {
+  name_prefix = "${var.name}-vpc-endpoint-s3"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = concat([module.vpc.vpc_cidr_block], var.secondary_cidrs)
+  }
+
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    ipv6_cidr_blocks = [module.vpc.vpc_ipv6_cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.name}-vpc-endpoint-s3"
+  }
+}
+
+module "vpc_endpoints" {
+  source = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+
+  vpc_id = module.vpc.vpc_id
+
+  endpoints = {
+    s3 = {
+      service = "s3"
+      subnet_ids = slice(module.vpc.private_subnets, 0, length(local.azs))
+      security_group_ids = [aws_security_group.vpc_endpoint_s3.id]
+      route_table_ids = concat(
+        module.vpc.private_route_table_ids,
+        module.vpc.public_route_table_ids
+      )
+      ip_address_type = "dualstack"
+      dns_options = {
+        dns_record_ip_type = "dualstack"
+      }
+      private_dns_enabled = true
+    }
+  }
+
+  tags = {
+    Name = "${var.name}-vpc-endpoints"
+  }
+}
